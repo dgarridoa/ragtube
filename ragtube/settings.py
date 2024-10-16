41a726a19dbd409271a4e6a93dc3066d72298699
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2024-10-16 15:41:46 -0300

    feat: add settings module

---
+++ ragtube/settings.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8"
+    )
+
+    youtube_api_key: SecretStr | None = None
+    db_user: SecretStr
+    db_password: SecretStr
+    db_host: SecretStr
+    db_port: SecretStr
+    db_name: SecretStr
+    api_user: SecretStr
+    api_password: SecretStr
+    api_host: SecretStr
+    api_port: SecretStr
+
+
+@lru_cache
+def get_settings():
+    settings = Settings()  # type: ignore
+    return settings
