350fdd8885f36ec3885574fd2ec4d0dd92cb747c
Diego Alberto Garrido Amaya <diego.garrido.6568@gmail.com>
2024-12-08 15:49:25 -0300

    refactor: use PROJECT_DIR to read files using global path

--- ragtube/settings.py
+++ ragtube/settings.py
@@ -1,12 +1,15 @@
+import os
 from functools import lru_cache
 
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from ragtube import PROJECT_DIR
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8"
+        env_file=os.path.join(PROJECT_DIR, ".env"), env_file_encoding="utf-8"
     )
 
     youtube_api_key: SecretStr
